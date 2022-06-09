a422072c7058e01d12356856729db90cf222c30b
Sergey Poznyakoff <gray@gnu.org>
2022-06-09 18:23:01 +0300

    Implement the cleanup goroutine

--- redis_transport.go
+++ redis_transport.go
@@ -2,19 +2,21 @@ package mercure
 
 import (
 	"context"
-	//"errors"
 	"github.com/go-redis/redis/v8"
 	"net/url"
 	"sync"
 	"fmt"
 	"encoding/json"
 	"go.uber.org/zap"
+	"time"
+	"strconv"
 )
 
 func init() { //nolint:gochecknoinits
 	RegisterTransportFactory("redis", NewRedisTransport)
 }
 
+// RedisTransport implements the TransportInterface for redis databases
 type RedisTransport struct {
 	sync.RWMutex
 	subscribers     *SubscriberList
@@ -24,35 +26,75 @@ type RedisTransport struct {
 	stream		string
 	closed          chan struct{}
 	closedOnce      sync.Once
+	eventTTL	time.Duration
+	cleanupInterval time.Duration
 }
 
-// redis://[user[:pass[@]HOST:[IP][/STREAM]
+// NewRedisTransport creates a new redis transport.
+// redis://[user[:pass[@]HOST:[IP][/STREAM][?event_ttl=N&cleanup_interval=N]
 func NewRedisTransport(u *url.URL, l Logger, tss *TopicSelectorStore) (Transport, error) { //nolint:ireturn
+	var (
+		eventTTL time.Duration = 24 * time.Hour
+		cleanupInterval time.Duration
+	)
+
+	q := u.Query()
+	if s := q.Get("event_ttl"); s != "" {
+		d, err := time.ParseDuration(s)
+		if err == nil {
+			if d < 0 {
+				eventTTL = - d
+			} else {
+				eventTTL = d
+			}
+		} else if c := l.Check(zap.ErrorLevel, "unparsable redis event TTL"); c != nil {
+			c.Write(zap.String("event-ttl", s))
+		}
+		q.Del("event_ttl")
+	}
+
+	if s := q.Get("cleanup_interval"); s != "" {
+		d, err := time.ParseDuration(s)
+		if err == nil {
+			cleanupInterval = d
+		} else if c := l.Check(zap.ErrorLevel, "unparsable redis cleanup interval"); c != nil {
+			c.Write(zap.String("cleanup-interval", s))
+		}
+		q.Del("cleanup_interval")
+	}
+	u.RawQuery = q.Encode()
+
 	options, err := redis.ParseURL(u.String())
 	if err != nil {
 		return nil, err
 	}
-	
-	rt := &RedisTransport{}
-	
-	rt.ctx = context.TODO()
-	
-	rt.client = redis.NewClient(options)
-	if err := rt.client.Ping(rt.ctx).Err(); err != nil {
+
+	ctx := context.TODO()
+	client := redis.NewClient(options)
+	if err := client.Ping(ctx).Err(); err != nil {
 		return nil, fmt.Errorf("Failed to connect to Redis: %w", err)
 	}
 
-	rt.logger = l
-	
+	rt := &RedisTransport{
+		subscribers: NewSubscriberList(1e5),
+		logger: l,
+		client: client,
+		ctx: ctx,
+		closed: make(chan struct{}),
+		eventTTL: eventTTL,
+		cleanupInterval: cleanupInterval,
+	}
+
 	if s := u.Path; s != "" {
 		rt.stream = s
 	} else {
 		rt.stream = "mercure"
 	}
 
-	rt.closed = make(chan struct{})
-	rt.subscribers = NewSubscriberList(1e5)
-	
+	if rt.cleanupInterval > 0 {
+		go rt.cleanup()
+	}
+
 	return rt, nil
 }
 
@@ -78,7 +120,7 @@ func (t *RedisTransport) storeUpdate(update *Update) error {
 
 	return err
 }
-	
+
 
 func (t *RedisTransport) Dispatch(update *Update) error {
 	select {
@@ -117,19 +159,19 @@ func (t *RedisTransport) dispatchHistory(s *Subscriber) (err error) {
 		dbg.Write(zap.String("RequestLastEventID", s.RequestLastEventID),
 			  zap.String("key", key))
 	}
-	
+
 	res, e := t.client.XReadStreams(t.ctx, t.stream, key).Result()
 	if e != nil {
 		err = fmt.Errorf("XREAD error: %w", e)
 		return
 	}
-	
+
 	if dbg := t.logger.Check(zap.DebugLevel, "dispatchHistory"); dbg != nil {
 		dbg.Write(zap.Int("Message count", len(res[0].Messages)))
 	}
 
 	responseLastEventID := EarliestLastEventID
-	
+
 	for _, msg := range res[0].Messages {
 		var update *Update
 		v, ok := msg.Values[`update`]
@@ -186,7 +228,7 @@ func (t *RedisTransport) AddSubscriber(s *Subscriber) error {
 	}
 
 	s.Ready()
-	return nil	
+	return nil
 }
 
 // RemoveSubscriber removes a new subscriber from the transport.
@@ -196,12 +238,12 @@ func (t *RedisTransport) RemoveSubscriber(s *Subscriber) error {
 		return ErrClosedTransport
 	default:
 	}
-	
+
 	t.Lock()
 	defer t.Unlock()
 	t.subscribers.Remove(s)
 
-	return nil	
+	return nil
 }
 
 // Close closes the Transport.
@@ -257,6 +299,91 @@ func (t *RedisTransport) GetSubscribers() (string, []*Subscriber, error) {
 		}
 	}
 	return lastEventID, subscribers, nil
+}
+
+func (t *RedisTransport) trim() error {
+	select {
+	case <-t.closed:
+		return ErrClosedTransport
+	default:
+	}
+
+	t.Lock()
+	defer t.Unlock()
+
+	minid := strconv.FormatInt(time.Now().Add(- t.eventTTL).UnixNano() / 1e6, 10)
+	if c := t.logger.Check(zap.DebugLevel, "Redis minID"); c != nil {
+		c.Write(zap.Int64("now", time.Now().UnixNano()/1e6),
+			zap.String("minID", minid))
+	}
+	res, err := t.client.XRange(t.ctx, t.stream, "0", minid).Result()
+	if err != nil {
+		if err == redis.Nil {
+			if c := t.logger.Check(zap.DebugLevel, "Redis trim"); c != nil {
+				c.Write(zap.String("message", "nothing to trim"))
+			}
+		} else if c := t.logger.Check(zap.ErrorLevel, "Can't find minID for trimming"); c != nil {
+			c.Write(zap.String("minID", minid), zap.Error(err))
+		}
+		return nil
+	}
+
+	var ids []string
+	for _, msg := range res {
+		var update *Update
+		v, ok := msg.Values[`update`]
+		if !ok {
+			if c := t.logger.Check(zap.ErrorLevel, "Malformed update message"); c != nil {
+				c.Write(zap.String("ID", msg.ID))
+			}
+		} else if upds, ok := v.(string); ok {
+			if err = json.Unmarshal([]byte(upds), &update); err != nil {
+				if c := t.logger.Check(zap.ErrorLevel, "Unmarshal error"); c != nil {
+					c.Write(zap.String("ID", msg.ID),
+						zap.String("update", upds),
+						zap.Error(err))
+				}
+			} else {
+				ids = append(ids, update.ID)
+			}
+		}
+	}
+
+	nIDs := int64(0)
+	nEvts := int64(0)
+
+	if (len(ids) > 0) {
+		if nIDs, err = t.client.Del(t.ctx, ids...).Result(); err != nil {
+			if c := t.logger.Check(zap.ErrorLevel, "Deleting IDs"); c != nil {
+				c.Write(zap.Int("Total IDs", len(ids)),
+					zap.Int64("Deleted IDs", nIDs),
+					zap.Error(err))
+			}
+		}
+	}
+
+	if nEvts, err = t.client.XTrimMinID(t.ctx, t.stream, minid).Result(); err != nil {
+		if c := t.logger.Check(zap.ErrorLevel, "Deleting entries"); c != nil {
+			c.Write(zap.Error(err))
+		}
+	}
+
+	if c := t.logger.Check(zap.InfoLevel, "Redis trim"); c != nil {
+		c.Write(zap.Int64("IDs deleted", nIDs),
+			zap.Int64("Events deleted", nEvts))
+	}
+
+	return nil
+}
+
+func (t *RedisTransport) cleanup() {
+	if c := t.logger.Check(zap.DebugLevel, "Redis cleanup"); c != nil {
+		c.Write(zap.Duration("cleanupInterval", t.cleanupInterval),
+			zap.Duration("envenTTL", t.eventTTL))
+	}
+	for t.trim() == nil {
+		time.Sleep(t.cleanupInterval)
+	}
 }
 
 // Interface guards.
