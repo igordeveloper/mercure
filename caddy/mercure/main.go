9af2c55fc1222eaeb548122c179cf8aacb26323c
Igor Kusmitsch <igor.kusmitsch@agu.gov.br>
2023-12-11 18:42:31 -0300

    update

--- caddy/mercure/main.go
+++ caddy/mercure/main.go
@@ -6,7 +6,7 @@ import (
 
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
-	_ "github.com/igordeveloper/mercure/caddy"
+	_ "github.com/igordeveloper/mercure/tree/main/caddy"
 )
 
 func main() {
